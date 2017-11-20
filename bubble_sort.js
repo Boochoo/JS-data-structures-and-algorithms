bc17b956d53d69a36c9242a905a263711552043a
boochoo <fromaddistohelsinki@gmail.com>
2017-11-21 00:12:07 +0200

    ArrayList, bubble_sort with for loop and another way of using insertion sort implemented

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,22 +1,50 @@
 var log = console.log;
 
-var bubbleSort = function (nums) {
-  do {
-    var swapped = false;
-    for (var index = 0; index < nums.length; index++) {
-      log(nums);
-      if (nums[index] > nums[index + 1]) {
-        var temp = nums[index];
-        nums[index] = nums[index + 1];
-        nums[index + 1] = temp;
+var bubbleSort = function(nums) {
+	var loop_num = 0;
+	do {
+		var swapped = false;
+		for (var index = 0; index < nums.length; index++) {
+			loop_num++; //runs 100 times
+			log(loop_num + ' ' + nums);
+			if (nums[index] > nums[index + 1]) {
+				var temp = nums[index];
+				nums[index] = nums[index + 1];
+				nums[index + 1] = temp;
 
-        swapped = true;
-      }    
-    }
-  } while (swapped){
-    log(nums);
-  }
+				swapped = true;
+			}
+		}
+	} while (swapped);
+	{
+		//log(nums);
+	}
 };
 
-var nums = [10,5,3,8,2,6,4,7,9,1];
+var nums = [ 10, 5, 3, 8, 2, 6, 4, 7, 9, 1 ],
+	nums_2 = nums.slice();
 bubbleSort(nums);
+
+//bubble sort in for loop
+/* THIS IS FASTER */
+
+var bubbleSort_2 = function(nums) {
+  var loop_num = 0;
+  var len = nums.length;
+  
+	for (let i = len - 1; i >= 0; i--) {
+		for (let j = len - i; j > 0; j--) {
+      
+      loop_num++; //runs 55 times
+			log(loop_num + ' ' + nums);
+      
+      if (nums[j] < nums[j - 1]) {
+				var currentItem = nums[j];
+				nums[j] = nums[j - 1];
+				nums[j - 1] = currentItem;
+			}
+		}
+	}
+};
+
+bubbleSort_2(nums_2);
---
+++ insertion_sort.js
@@ -0,0 +1,41 @@
+var log = console.log;
+
+var insertion_sort = function(nums) {
+	var loop_num = 0;
+
+	for (let i = 0, len = nums.length; i < len; i++) {
+		for (let j = 0; j < i; j++) {
+			loop_num++; //runs 45 times
+      log(loop_num + ' ' + nums);
+      
+			if (nums[i] < nums[j]) {
+				var spliced = nums.splice(i, 1);
+				nums.splice(j, 0, spliced[0]);
+			}
+		}
+	}
+};
+
+var nums = [ 10, 5, 3, 8, 2, 6, 4, 7, 9, 1 ], nums_2 = nums.slice();
+
+insertion_sort(nums);
+
+var insertion_sort_2 = function(unsortedList) {
+	let len = unsortedList.length, loop_num = 0;
+
+	for (let i = 0; i < len; i++) {
+		const currentElement = unsortedList[i]; //copy of current element
+
+		/*Check through the sorted part and compare with the number in currentElement. If large, shift the number*/
+		for (let j = i - 1; j >= 0 && (unsortedList[j] > currentElement); j--) {
+
+      loop_num++; //runs 26 times
+      log(loop_num + ' ' + unsortedList);
+			unsortedList[j + 1] = unsortedList[j]; //shift the number
+      unsortedList[j] = currentElement;
+    }   
+	}
+};
+
+insertion_sort_2(nums_2);  
+log(nums); 
